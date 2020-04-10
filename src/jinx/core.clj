74f51967e8d2c8b863f20b2d605bd17a0076f8c4
Mourjo Sen <mourjo@helpshift.com>
2020-04-10 10:30:12 +0530

    Initial commit

---
+++ src/jinx/core.clj
@@ -0,0 +1,6 @@
+(ns jinx.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/jinx/core_test.clj
@@ -0,0 +1,7 @@
+(ns jinx.core-test
+  (:require [clojure.test :refer :all]
+            [jinx.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
