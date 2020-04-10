c3ca68f4ebcde6d2edc26c1135f7b1665b378c7d
Mourjo Sen <mourjo@helpshift.com>
2020-04-10 12:30:50 +0530

    Basic source code

--- src/jinx/core.clj
+++ src/jinx/core.clj
@@ -1,6 +1,61 @@
-(ns jinx.core)
+(ns jinx.core
+  (:require [cheshire.core :as cc])
+  (:import java.time.ZoneId
+           java.time.LocalDateTime))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn fetch
+  [url & {:keys [parser]
+          :or {parser #(cc/parse-string % keyword)}}]
+  (loop [i 0]
+    (Thread/sleep (* 100 i))
+    (let [[code result] (try
+                        [::success
+                         (-> url slurp parser)]
+                        (catch Exception e
+                          [::failure
+                           e]))]
+      (cond
+        (= code ::success) result
+        (< i 5) (recur (inc i))
+        :else (throw result)))))
+
+
+(defn remote-timezones
+  []
+  (set (fetch "https://worldtimeapi.org/api/timezone")))
+
+
+(defn remote-api-info
+  []
+  (fetch "http://worldtimeapi.org/api/" :parser identity))
+
+
+(defn system-timezones
+  []
+  (ZoneId/getAvailableZoneIds))
+
+
+(defn remote-time
+  [tz]
+  (-> "https://worldtimeapi.org/api/timezone/"
+      (str tz)
+      fetch
+      :datetime))
+
+
+(defn system-time
+  [tz]
+  (str (LocalDateTime/now (ZoneId/of tz))))
+
+
+(defn estimate-remote-time-error
+  ([]
+   (estimate-remote-time-error 5))
+  ([n]
+   (let [rtts (for [tz (take n (shuffle (remote-timezones)))]
+                (let [start (System/nanoTime)]
+                  (try (remote-time tz)
+                       (catch Exception _ 0))
+                  (/ (double (- (System/nanoTime) start)) 1000000.0)))]
+     (/ (double (/ (apply + rtts) n)) 2.0))))
